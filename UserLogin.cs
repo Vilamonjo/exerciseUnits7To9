af55a59b4f93340628ef5788767852b804301359
vilamonjo <vilamonjo@gmail.com>
2018-11-21 23:57:16 +0100

    Second exercice done on C# (Exercises for units 7 to 9)

---
+++ UserLogin.cs
@@ -0,0 +1,54 @@
+﻿//Jose Vilaplana
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string user, password;
+        bool done = false;
+        byte count = 0;
+
+        //CORRECT USER -> Root
+        //CORRECT PASS -> 1234
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the user");
+            Console.Write("USER: ");
+            user = Console.ReadLine();
+            Console.WriteLine("Enter the password");
+            Console.Write("PASSWORD: ");
+            password = Console.ReadLine();
+
+            if (user == "Root" && password == "1234")
+            {
+                done = true;
+            }
+            else
+            {
+                Console.WriteLine("User or Password incorrect");
+                Console.ReadLine();
+            }
+                
+            count++;
+        } while (!done && count < 3);
+
+        if (!done || count == 3)
+        {
+            Console.Clear();
+            Console.WriteLine("Permission Denied.");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("You're in!.");
+            Console.ReadLine();
+        }
+            
+            
+
+    }
+}
